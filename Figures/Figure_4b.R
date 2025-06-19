5f6d5e09503300e980849c2dea0720e7ff2a9a30
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-19 17:04:12 -0500

    Figure_4b.R

--- Figures/Figure_4b.R
+++ Figures/Figure_4b.R
@@ -1,5 +1,45 @@
-set.seed(2023)
+
+set.seed(20203)
 library(ggplot2)
+
+## CosMx and RNAseq
+ggscatter(ST, x = "D_C_genes_S", y = "C_MESO2_genes_S", 
+                add = "reg.line", conf.int = TRUE, 
+                cor.coef = TRUE, cor.method = "pearson",
+                add.params = list(color = "red",fill = "lightgray"),
+                xlab = "RNAseq", ylab = "CosMx MESO2")+
+  theme(text = element_text(size = 18))+
+  geom_label(label=MESO2_D_M$genes)+
+  ggtitle('MESO2 (n=960)')
+
+
+## MERFISH and RNAseq
+ggscatter(ST, x = "D_M_genes_S", y = "M_MESO2_genes_S", 
+                add = "reg.line", conf.int = TRUE, 
+                cor.coef = TRUE, cor.method = "pearson",
+                add.params = list(color = "red",fill = "lightgray"),
+                xlab = "RNAseq", ylab = "Merfish MESO2")+
+  theme(text = element_text(size = 18))+
+  geom_label(label=MESO2_D_M$genes)+
+  ggtitle('MESO2 (n=499)')
+
+## Xenium_UM and RNAseq
+ggscatter(ST, x = "D_X_genes_S", y = "X_MESO2_genes_S", 
+                add = "reg.line", conf.int = TRUE, 
+                cor.coef = TRUE, cor.method = "pearson",
+                add.params = list(color = "red",fill = "lightgray"),
+                xlab = "RNAseq", ylab = "Xenium_UM MESO2")+
+  theme(text = element_text(size = 18))+
+  ggtitle('MESO2 (n=339)')
+
+## Xenium_UM and RNAseq
+ggscatter(ST, x = "D_X_new_genes_S", y = "X_MESO2_new_genes_S", 
+                add = "reg.line", conf.int = TRUE, 
+                cor.coef = TRUE, cor.method = "pearson",
+                add.params = list(color = "red",fill = "lightgray"),
+                xlab = "RNAseq", ylab = "Xenium_MM MESO2")+
+  theme(text = element_text(size = 18))+
+  ggtitle('MESO2 (n=339)')
 
 
 
