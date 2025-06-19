e9dc7565d62c94b48d688a14b55aed27a1848bc3
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-19 17:18:08 -0500

    Figure_5b.R

--- Figures/Figure_5b.R
+++ Figures/Figure_5b.R
@@ -6,7 +6,7 @@ library(ggplot2)
 ggscatter(ST, x = "D_C_genes_S", y = "C_MESO2_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "CosMx MESO2")+
   theme(text = element_text(size = 18))+
   geom_label(label=MESO2_D_M$genes)+
@@ -17,7 +17,7 @@ ggscatter(ST, x = "D_C_genes_S", y = "C_MESO2_genes_S",
 ggscatter(ST, x = "D_M_genes_S", y = "M_MESO2_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "Merfish MESO2")+
   theme(text = element_text(size = 18))+
   geom_label(label=MESO2_D_M$genes)+
@@ -27,7 +27,7 @@ ggscatter(ST, x = "D_M_genes_S", y = "M_MESO2_genes_S",
 ggscatter(ST, x = "D_X_genes_S", y = "X_MESO2_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "Xenium_UM MESO2")+
   theme(text = element_text(size = 18))+
   ggtitle('MESO2 (n=336)')
@@ -36,7 +36,7 @@ ggscatter(ST, x = "D_X_genes_S", y = "X_MESO2_genes_S",
 ggscatter(ST, x = "D_X_new_genes_S", y = "X_MESO2_new_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "Xenium_MM MESO2")+
   theme(text = element_text(size = 18))+
   ggtitle('MESO2 (n=336)')
