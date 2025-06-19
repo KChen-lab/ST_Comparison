597aee0a7f7d111d6c26a5af7971ad9fa0baf577
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-19 17:18:28 -0500

    Figure_5a.R

--- Figures/Figure_5a.R
+++ Figures/Figure_5a.R
@@ -6,7 +6,7 @@ library(ggplot2)
 ggscatter(ST, x = "D_C_genes_S", y = "C_ICON2_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "CosMx ICON2")+
   theme(text = element_text(size = 18))+
   geom_label(label=ICON2_D_M$genes)+
@@ -17,7 +17,7 @@ ggscatter(ST, x = "D_C_genes_S", y = "C_ICON2_genes_S",
 ggscatter(ST, x = "D_M_genes_S", y = "M_ICON2_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "Merfish ICON2")+
   theme(text = element_text(size = 18))+
   geom_label(label=ICON2_D_M$genes)+
@@ -27,7 +27,7 @@ ggscatter(ST, x = "D_M_genes_S", y = "M_ICON2_genes_S",
 ggscatter(ST, x = "D_X_genes_S", y = "X_ICON2_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "Xenium_UM ICON2")+
   theme(text = element_text(size = 18))+
   ggtitle('ICON2 (n=336)')
@@ -36,7 +36,7 @@ ggscatter(ST, x = "D_X_genes_S", y = "X_ICON2_genes_S",
 ggscatter(ST, x = "D_X_new_genes_S", y = "X_ICON2_new_genes_S", 
                 add = "reg.line", conf.int = TRUE, 
                 cor.coef = TRUE, cor.method = "pearson",
-                add.params = list(color = "red",fill = "lightgray"),
+                add.params = list(color = "blue",fill = "lightgray"),
                 xlab = "DSP WTA", ylab = "Xenium_MM ICON2")+
   theme(text = element_text(size = 18))+
   ggtitle('ICON2 (n=336)')
