427ef4cd86e1a15bd61277a23514077d6f420994
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-06-19 17:51:43 -0500

    Supp.Fig_11b.R

---
+++ Figures/Supp.Fig_11b.R
@@ -0,0 +1,16 @@
+set.seed(2023)
+library(ggplot2)
+
+library(reshape2)
+melted_cormat <- melt(data, na.rm = TRUE)
+
+ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4)+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0.5, limit = c(0,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ ylab("Merfish MESO")+xlab("Xenium-UM MESO")+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()
